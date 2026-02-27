b5b1afb96285011587426919919349f8e668dbe6
Sebastián Salazar <sebastian.salazar.solano@gmail.com>
2026-02-27 16:09:22 -0600

    chore: fix vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,16 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html'],
-      exclude: ['**/node_modules/**', '**/dist/**', '**/*.test.ts'],
+      include: ['packages/*/src/**/*.ts'],
+      exclude: [
+        '**/node_modules/**',
+        '**/dist/**',
+        '**/*.test.ts',
+        '**/__fixtures__/**',
+        '**/vitest.config.ts',
+        'benchmarks/**',
+        'examples/**',
+      ],
       thresholds: {
         lines: 70,
         functions: 70,
